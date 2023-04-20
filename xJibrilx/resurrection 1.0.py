3b7295d225117198fdda187ed042f9582355025f
xJibrilx <129985081+xJibrilx@users.noreply.github.com>
2023-04-20 09:48:00 -0700

    Create resurrection 1.0.py
    Changes to the way the count() function works.
    It now keeps record of the loop iteration and the decimal number simultaneously.

---
+++ xJibrilx/resurrection 1.0.py
@@ -0,0 +1,14 @@
+import random
+import itertools
+value = random.random()
+i = value
+def resurrection():
+    print("Jesus Is Lord") # yes he is
+def value():
+    return random.uniform(0,1)# random selection between 0 and 1
+while i != 1: # dice roll of the code
+    for x in itertools.count():  # the counter function +1 increment
+        print(value())
+        if (value()) >= 1: # important to note when dealing with decimals to use or equals
+            print(resurrection())
+        print(x)
